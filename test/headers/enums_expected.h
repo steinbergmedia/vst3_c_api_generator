29404c407ad87d10c39ba3c53b16afb04f784c7c
Frank Sauerland <F.Sauerland@steinberg.de>
2022-08-18 16:47:46 +0200

    adjust unit tests

--- test/headers/enums_expected.h
+++ test/headers/enums_expected.h
@@ -1,21 +1,21 @@
 typedef enum
 {
-Steinberg_IBStream_IStreamSeekMode_kIBSeekSet = 0,
-Steinberg_IBStream_IStreamSeekMode_kIBSeekCur,
-Steinberg_IBStream_IStreamSeekMode_kIBSeekEnd
+    Steinberg_IBStream_IStreamSeekMode_kIBSeekSet = 0,
+    Steinberg_IBStream_IStreamSeekMode_kIBSeekCur,
+    Steinberg_IBStream_IStreamSeekMode_kIBSeekEnd
 } Steinberg_IBStream_IStreamSeekMode;
 
 typedef enum
 {
-Steinberg_Vst_MediaTypes_kAudio = 0,
-Steinberg_Vst_MediaTypes_kEvent,
-Steinberg_Vst_MediaTypes_kNumMediaTypes
+    Steinberg_Vst_MediaTypes_kAudio = 0,
+    Steinberg_Vst_MediaTypes_kEvent,
+    Steinberg_Vst_MediaTypes_kNumMediaTypes
 } Steinberg_Vst_MediaTypes;
 
 typedef enum
 {
-Steinberg_Vst_IAutomationState_AutomationStates_kNoAutomation = 0,
-Steinberg_Vst_IAutomationState_AutomationStates_kReadState = 1 << 0,
-Steinberg_Vst_IAutomationState_AutomationStates_kWriteState = 1 << 1,
-Steinberg_Vst_IAutomationState_AutomationStates_kReadWriteState = Steinberg_Vst_IAutomationState_AutomationStates_kReadState | Steinberg_Vst_IAutomationState_AutomationStates_kWriteState
+    Steinberg_Vst_IAutomationState_AutomationStates_kNoAutomation = 0,
+    Steinberg_Vst_IAutomationState_AutomationStates_kReadState = 1 << 0,
+    Steinberg_Vst_IAutomationState_AutomationStates_kWriteState = 1 << 1,
+    Steinberg_Vst_IAutomationState_AutomationStates_kReadWriteState = Steinberg_Vst_IAutomationState_AutomationStates_kReadState | Steinberg_Vst_IAutomationState_AutomationStates_kWriteState
 } Steinberg_Vst_IAutomationState_AutomationStates;
--- test/headers/structs_expected.h
+++ test/headers/structs_expected.h
@@ -1,4 +1,5 @@
-struct Steinberg_PFactoryInfo {
+struct Steinberg_PFactoryInfo
+{
     Steinberg_char8 vendor[256 << 4 >> 3 | 1];
     Steinberg_char8 url[256];
     Steinberg_char8 email[256 << 4];
