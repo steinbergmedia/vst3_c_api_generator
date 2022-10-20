87bf2db1959d550a633827bc9c34c4998fec2093
Arne Scheffler <a.scheffler@steinberg.de>
2022-10-20 11:52:10 +0200

    [new] add license

--- c_gain_example/cgain.c
+++ c_gain_example/cgain.c
@@ -6,6 +6,14 @@
 // Description : <#Description#>
 //------------------------------------------------------------------------
 
+//-----------------------------------------------------------------------------
+// This file is part of a Steinberg SDK. It is subject to the license terms
+// in the LICENSE file found in the top-level directory of this distribution
+// and at www.steinberg.net/sdklicenses. 
+// No part of the SDK, including this file, may be copied, modified, propagated,
+// or distributed except according to the terms contained in the LICENSE file.
+//-----------------------------------------------------------------------------
+
 #include <math.h>
 #include <memory.h>
 #include <stdio.h>
--- generate_header/scripts/test/test_conversion.py
+++ generate_header/scripts/test/test_conversion.py
@@ -1,3 +1,11 @@
+#-----------------------------------------------------------------------------
+# This file is part of a Steinberg SDK. It is subject to the license terms
+# in the LICENSE file found in the top-level directory of this distribution
+# and at www.steinberg.net/sdklicenses. 
+# No part of the SDK, including this file, may be copied, modified, propagated,
+# or distributed except according to the terms contained in the LICENSE file.
+#-----------------------------------------------------------------------------
+
 import re
 import unittest
 from pathlib import Path
